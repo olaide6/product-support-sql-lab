b7bb646bc8f027c590cfbbd1eea3e333775e6c3d
omoyele olaide <laide.omoyele@gmail.com>
2026-07-27 03:47:42 +0100

    Add query for inactive subscriptions with customer details

--- queries/inactive-subscriptions.sql
+++ queries/inactive-subscriptions.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    s.subscription_id,
+    c.full_name,
+    c.email,
+    s.status,
+    s.renewal_date
+FROM subscriptions s
+JOIN customers c
+ON s.customer_id = c.customer_id
+WHERE s.status = 'Inactive';
